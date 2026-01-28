1dd1752a5f82d2707fd473896db7e3baa55633b7
bjoernsauerweinbrs-spec <bjoern.sauerwein.brs@gmail.com>
2026-01-28 16:58:25 +0100

    Update eventbus.js

--- js/modules/eventbus.js
+++ js/modules/eventbus.js
@@ -1,12 +1,26 @@
+/**
+ * TONI 2.0 - EventBus
+ * Das zentrale Nervensystem für die Kommunikation
+ */
 window.ToniEvents = {
     events: {},
+
+    /**
+     * Ein Event abonnieren (Zuhören)
+     */
     on(eventName, fn) {
         this.events[eventName] = this.events[eventName] || [];
         this.events[eventName].push(fn);
+        console.log(`[EventBus] Abo für: ${eventName}`);
     },
+
+    /**
+     * Ein Event auslösen (Senden)
+     */
     emit(eventName, data) {
         if (this.events[eventName]) {
             this.events[eventName].forEach(fn => fn(data));
+            console.log(`[EventBus] Sende: ${eventName}`, data);
         }
     }
 };
