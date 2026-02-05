012266b960d745e385770051e63ca6fd162c99dc
bjoernsauerweinbrs-spec <bjoern.sauerwein.brs@gmail.com>
2026-02-05 10:12:20 +0100

    Update event-bus.js

--- logic/event-bus.js
+++ logic/event-bus.js
@@ -1,12 +1,15 @@
+/**
+ * TONI 2.0 - EVENT BUS
+ * Das zentrale Nervensystem für die Kommunikation der Module.
+ */
 window.ToniEvents = {
     listeners: {},
-    on: function(event, handler) {
+    on: function(event, callback) {
         if (!this.listeners[event]) this.listeners[event] = [];
-        this.listeners[event].push(handler);
+        this.listeners[event].push(callback);
     },
-    emit: function(event, payload) {
-        console.log(`[EventBus] ${event} gefeuert.`);
-        (this.listeners[event] || []).forEach(h => h(payload));
+    emit: function(event, data) {
+        if (!this.listeners[event]) return;
+        this.listeners[event].forEach(callback => callback(data));
     }
-}; 
- 
+};
