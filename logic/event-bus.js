28547979417f0bf39a96b48f0e130ba10ad8f6f7
bjoernsauerweinbrs-spec <bjoern.sauerwein.brs@gmail.com>
2026-02-05 09:36:57 +0100

    Create event-bus.js

---
+++ logic/event-bus.js
@@ -0,0 +1,11 @@
+window.ToniEvents = {
+    listeners: {},
+    on: function(event, handler) {
+        if (!this.listeners[event]) this.listeners[event] = [];
+        this.listeners[event].push(handler);
+    },
+    emit: function(event, payload) {
+        console.log(`[EventBus] ${event} gefeuert.`);
+        (this.listeners[event] || []).forEach(h => h(payload));
+    }
+};
