8779b9c4bfc336e856aaf57f71dcff3912db97ca
bjoernsauerweinbrs-spec <bjoern.sauerwein.brs@gmail.com>
2026-02-07 07:46:41 +0100

    Update database.js

--- logic/database.js
+++ logic/database.js
@@ -14,8 +14,15 @@ window.Database = {
     createDemoTeam() {
         const names = ["Max Master", "Lukas Wall", "Toni Technic", "Marc Speed", "Sven Safe", "Finn Flügel", "Ben Beißer", "Leo Luft", "Mika Mitti", "Sam Solo", "Jan Jäger", "Oli Ordnung", "Paul Pass", "Kalle Kante", "Nico Netz", "Dennis Dribbel", "Uli Umkehr", "Basti Ball", "Rene Räumer", "Flo Flanke"];
         this.players = names.map((name, i) => ({
-            id: i + 1, name, pos: i === 4 ? "TW" : "FELD", rat: 85, 
-            pac: 80, sho: 70, pas: 75, heart: 72, km: 0, present: i < 11, img: null
+            id: i + 1, 
+            name: name, 
+            pos: i === 4 ? "TW" : "FELD", 
+            rat: 85, 
+            pac: 80, sho: 70, pas: 75,
+            heart: 65, // Puls Standard
+            km: 0.0,   // Kilometer Standard
+            present: i < 11, 
+            img: null
         }));
         this.save();
     },
@@ -23,10 +30,6 @@ window.Database = {
     updatePlayer(id, key, val) {
         const p = this.players.find(x => x.id === id);
         if (p) { p[key] = val; this.save(); }
-    },
-    togglePresence(id) {
-        const p = this.players.find(x => x.id === id);
-        if (p) { p.present = !p.present; this.save(); if(window.arena) window.arena.syncFromDatabase(); }
     }
 };
 window.Database.init();
