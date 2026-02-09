771597b11f1a88c56a2998bfa45be210e3e68913
bjoernsauerweinbrs-spec <bjoern.sauerwein.brs@gmail.com>
2026-02-09 11:29:33 +0100

    Create youth-presets.js

---
+++ data/youth-presets.js
@@ -0,0 +1,7 @@
+window.YouthPresets = {
+    musterspieler: [
+        { id: "j1", name: "Max Mustermann", jahrgang: 2018, jugend: "G-Jugend", rating: "Junior" },
+        { id: "j2", name: "Leon Dribbler", jahrgang: 2017, jugend: "F-Jugend", rating: "Junior" },
+        { id: "j3", name: "Sarah Superkick", jahrgang: 2018, jugend: "G-Jugend", rating: "Talent" }
+    ]
+};
